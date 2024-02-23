e837b45097955f92f5bb7cedd9eb017d9f21f735
오민준 <sige_tank@naver.com>
2024-02-23 22:06:24 +0900

    [FEAT] nav가 필요없는 페이지 예외사항으로 추가

--- mst/src/app/(mainpage)/layout.tsx
+++ mst/src/app/(mainpage)/layout.tsx
@@ -1,21 +1,33 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import "@/styles/globals.css";
 import Nav from "@/components/common/Nav/Nav";
 import { css } from "@emotion/react";
 
-export default function RootLayout({
+export default function MainPageLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body>
+}) {
+  const pathname = usePathname();
+
+  // layout이 필요없는 router주소는 여기에 넣어준다.
+  if (pathname === "/") {
+    return (
+      <div>
         <div css={contentWrapperCSS}>{children}</div>
         <Nav />
-      </body>
-    </html>
+      </div>
+    );
+  }
+
+  // 그 외의 경우에는 Nav 컴포넌트와 함께 렌더링
+  return (
+    <div>
+      <div css={contentWrapperCSS}>{children}</div>
+      <Nav />
+    </div>
   );
 }
 
