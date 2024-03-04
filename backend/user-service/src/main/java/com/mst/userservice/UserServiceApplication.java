99e3e1cba9ea8be4941a82bc5ec2327d99b43c52
김부경 <sa01023@naver.com>
2024-03-04 22:57:39 +0900

    [FEAT] discovery client 설정

--- backend/user-service/src/main/java/com/mst/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/mst/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.mst.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // 유레카 클라이언트로 등록
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
