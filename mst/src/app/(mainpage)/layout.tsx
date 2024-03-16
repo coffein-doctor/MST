a2dde4f705287a002eb7c2622cfc42c03c76be40
민준 <sige_tank@naver.com>
2024-03-16 16:37:50 +0900

    [FEAT] layout에서 더 이상 topBar를 출력하지 않게 변경

--- mst/src/app/(mainpage)/layout.tsx
+++ mst/src/app/(mainpage)/layout.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import "@/styles/globals.css";
 import Nav from "@/components/common/Nav/Nav";
-import TopBar from "@/components/common/TopBar/TopBar";
 import { css } from "@emotion/react";
 
 export default function MainPageLayout({
@@ -11,95 +9,10 @@ export default function MainPageLayout({
 }: {
   children: React.ReactNode;
 }) {
-  interface PagesConfig {
-    [key: string]: {
-      showTopBar: boolean;
-      TopBarType?: "basic" | "select" | "search";
-      title?: string;
-      showNavBar: boolean;
-      selectOptions?: { value: string; label: string }[];
-    };
-  }
-
-  const pathname = usePathname();
-
-  const pagesConfig: PagesConfig = {
-    "/home": {
-      showTopBar: true,
-      TopBarType: "basic",
-      title: "홈페이지",
-      showNavBar: true,
-    },
-    "/stats.*": {
-      showTopBar: true,
-      TopBarType: "select",
-      selectOptions: [
-        { value: "/stats/daily", label: "일간" },
-        { value: "/stats/monthly", label: "월간" },
-      ],
-      showNavBar: true,
-    },
-
-    "/beverage": {
-      showTopBar: false,
-      showNavBar: false,
-    },
-
-    "/beverage/water": {
-      showTopBar: true,
-      TopBarType: "basic",
-      title: "물",
-      showNavBar: false,
-    },
-
-    "/beverage.*": {
-      showTopBar: true,
-      TopBarType: "basic",
-      title: "음료 등록",
-      showNavBar: false,
-    },
-
-    "/community": {
-      showTopBar: true,
-      TopBarType: "select",
-      selectOptions: [
-        { value: "/community", label: "커뮤니티" },
-        { value: "/community/feed", label: "피드" },
-      ],
-      showNavBar: true,
-    },
-
-    "/community.*": {
-      showTopBar: true,
-      TopBarType: "select",
-      selectOptions: [
-        { value: "/community", label: "커뮤니티" },
-        { value: "/community/feed", label: "피드" },
-      ],
-      showNavBar: true,
-    },
-
-    "/mypage": {
-      showTopBar: false,
-      showNavBar: true,
-    },
-  };
-
-  const currentPageConfig = Object.entries(pagesConfig).find(([key]) => {
-    return new RegExp(`^${key.replace("*", ".*")}$`).test(pathname);
-  })?.[1];
-
   return (
     <div>
-      {currentPageConfig?.showTopBar && (
-        <TopBar
-          content={currentPageConfig.title}
-          type={currentPageConfig.TopBarType}
-          selectOptions={currentPageConfig.selectOptions}
-        />
-      )}
       <div css={contentWrapperCSS}>{children}</div>
-      {currentPageConfig?.showNavBar && <Nav />}
+      <Nav />
     </div>
   );
 }
