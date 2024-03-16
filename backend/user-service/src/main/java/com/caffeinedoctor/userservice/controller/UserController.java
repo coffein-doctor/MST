a8132ecc6bf76edc05b60a639cae64524cf3a135
김부경 <sa01023@naver.com>
2024-03-16 23:09:33 +0900

    [FEAT] API GATEWAY 경로 수정

--- backend/user-service/src/main/java/com/caffeinedoctor/userservice/controller/UserController.java
+++ backend/user-service/src/main/java/com/caffeinedoctor/userservice/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.caffeinedoctor.userservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user-service")
 public class UserController {
 
     @GetMapping("/")
