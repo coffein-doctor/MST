4a104c4ce7493eb0100d4961c577931c63dd5ad9
김부경 <sa01023@naver.com>
2024-03-18 20:32:38 +0900

    [FEAT] API GATEWAY 필터 적용

--- backend/beverage-service/src/main/java/com/caffeinedoctor/beverageservice/contoller/BeverageController.java
+++ backend/beverage-service/src/main/java/com/caffeinedoctor/beverageservice/contoller/BeverageController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableDiscoveryClient
 public class BeverageController {
     @GetMapping("/")
     public String getServiceName(){
