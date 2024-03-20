facd2d01c136c5fdeeb09fa564e3659d55717fa4
김부경 <sa01023@naver.com>
2024-03-20 23:21:05 +0900

    [FEAT] API GATEWAY header 삽입

---
+++ backend/community-service/src/main/java/com/caffeinedoctor/communityservice/controller/CommunityServiceController.java
@@ -0,0 +1,22 @@
+package com.caffeinedoctor.communityservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class CommunityServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Community service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("community-request") String header) {
+        log.info(header);
+        return "Hello World in Community Service";
+    }
+}
