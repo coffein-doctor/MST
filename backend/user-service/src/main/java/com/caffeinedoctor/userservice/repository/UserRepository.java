aa47ce32aea72aa18f877cda6e8643dbd1a8a234
김부경 <sa01023@naver.com>
2024-03-26 01:09:32 +0900

    [FEAT] user Service 구성

--- backend/user-service/src/main/java/com/caffeinedoctor/userservice/repository/UserRepository.java
+++ backend/user-service/src/main/java/com/caffeinedoctor/userservice/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.caffeinedoctor.userservice.repository;
 
 import com.caffeinedoctor.userservice.entitiy.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+// paRepository는 페이징, 정렬, 쿼리 메서드와 같은 추가 기능을 제공
+// public interface UserRepository extends JpaRepository<User, Long> {
+// CrudRepository는 기본적인 CRUD 작업만을 제공
+public interface UserRepository extends CrudRepository<User, Long> {
+    // CrudRepository<연동 될 entity, entity의 기본키 타입>
 }
