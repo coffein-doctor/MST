8031a66fd5f2a141bc3357cd5baa9424298b3fc9
김부경 <sa01023@naver.com>
2024-04-08 22:04:32 +0900

    [FEAT] 소셜로그인 API swagger 설명 수정

--- backend/user-service/src/main/java/com/caffeinedoctor/userservice/controller/OAuth2Controller.java
+++ backend/user-service/src/main/java/com/caffeinedoctor/userservice/controller/OAuth2Controller.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "OAuth2 소셜로그인", description = "소셜로그인 API")
+@Tag(name = "OAuth2 소셜 로그인", description = "OAuth2를 사용한 소셜 로그인 API")
 @RestController
 @RequestMapping("/oauth2")
 @RequiredArgsConstructor // 파이널 필드만 가지고 생성사 주입 함수 만듬 (따로 작성할 필요 없다.)
@@ -21,7 +21,7 @@ public class OAuth2Controller {
 
     @Operation(
             summary = "소셜 로그인",
-            description = "해당 소셜 로그인 페이지로 이동합니다."
+            description = "소셜 로그인 페이지로 이동합니다. 현재는 '카카오 로그인'이 구현되어 있습니다."
     )
     @ApiResponse(
             responseCode = "200",
