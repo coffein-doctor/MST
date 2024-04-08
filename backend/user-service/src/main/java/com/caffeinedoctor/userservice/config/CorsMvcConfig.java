1033c1ed53100ad0b74c96f39a89cc1b828a43f7
김부경 <sa01023@naver.com>
2024-04-09 00:28:26 +0900

    [FEAT] cors 설정

---
+++ backend/user-service/src/main/java/com/caffeinedoctor/userservice/config/CorsMvcConfig.java
@@ -0,0 +1,17 @@
+package com.caffeinedoctor.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        //모든 경로에 매핑
+        corsRegistry.addMapping("/**")
+                .exposedHeaders("Set-Cookie") //쿠키 헤더로 전달
+                .allowedOrigins("http://localhost:3000"); //프론트 서버 주소
+    }
+}
--- backend/user-service/src/main/java/com/caffeinedoctor/userservice/security/SecurityConfig.java
+++ backend/user-service/src/main/java/com/caffeinedoctor/userservice/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.caffeinedoctor.userservice.security.jwt.JWTFilter;
 import com.caffeinedoctor.userservice.security.jwt.JWTUtil;
 import com.caffeinedoctor.userservice.security.oauth2.CustomSuccessHandler;
 import com.caffeinedoctor.userservice.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 // 시큐리티 커스텀 config를 작성하여 특정 경로에 대해 접근을 막는다.
 @Configurable
@@ -48,6 +53,30 @@ public class SecurityConfig {
     // 특정 경로에 대해서 보안 설정하기
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        //jwt를 위한 설정
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        // 프론트 서버 주소
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        // get, post, ... 모든 요청에 허용
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        // 모든 헤더 값 허용
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
 
         // 비활성화
         http
