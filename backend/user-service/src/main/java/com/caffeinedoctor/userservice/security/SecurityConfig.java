c52be54d5833fd13b2005ab376d73a156117014d
김부경 <sa01023@naver.com>
2024-04-15 21:47:12 +0900

    [FEAT] cors 주소 허용

--- backend/user-service/src/main/java/com/caffeinedoctor/userservice/security/SecurityConfig.java
+++ backend/user-service/src/main/java/com/caffeinedoctor/userservice/security/SecurityConfig.java
@@ -68,6 +68,7 @@ public class SecurityConfig {
                         CorsConfiguration configuration = new CorsConfiguration();
                         // 프론트 서버 주소
 //                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.addAllowedOrigin("*");
                         // get, post, ... 모든 요청에 허용
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
