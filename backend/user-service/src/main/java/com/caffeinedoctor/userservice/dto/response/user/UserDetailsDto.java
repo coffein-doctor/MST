739e76d6685c1d1744112eca5d2349e267b4576e
김부경 <sa01023@naver.com>
2024-04-17 14:59:58 +0900

    [FEAT] 회원 등록 로직 수정

---
+++ backend/user-service/src/main/java/com/caffeinedoctor/userservice/dto/response/user/UserDetailsDto.java
@@ -0,0 +1,50 @@
+package com.caffeinedoctor.userservice.dto.response.user;
+
+import com.caffeinedoctor.userservice.enums.ActivityLevel;
+import com.caffeinedoctor.userservice.enums.Gender;
+import com.caffeinedoctor.userservice.enums.UserStatus;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+public class UserDto {
+    public Long id;
+    public String username;
+    public String email;
+    public UserStatus status;
+    public String nickname;
+    public LocalDate birth;
+    public Gender gender;
+    public int height;
+    public int weight;
+    public ActivityLevel activityLevel;
+    public String profileImageUrl;
+    public String introduction;
+    public LocalDateTime signUpDate;
+    public LocalDateTime loginDate;
+
+    // 생성자, 게터, 세터 등 필요한 메서드 추가
+    @Builder
+    public UserDto(Long id, String username, String email, UserStatus status, String nickname,
+                LocalDate birth, Gender gender, int height, int weight, ActivityLevel activityLevel,
+                String profileImageUrl, String introduction, LocalDateTime signUpDate, LocalDateTime loginDate) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.status = status;
+        this.nickname = nickname;
+        this.birth = birth;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.activityLevel = activityLevel;
+        this.profileImageUrl = profileImageUrl;
+        this.introduction = introduction;
+        this.signUpDate = signUpDate;
+        this.loginDate = loginDate;
+    }
+
+}
