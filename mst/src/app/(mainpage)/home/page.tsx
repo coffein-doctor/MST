ad8c4967d3c975fe9dfaec0fd9d47324949bd567
오민준 <sige_tank@naver.com>
2024-05-08 13:40:12 +0900

    feat : 빌드 에러 수정

--- mst/src/app/(mainpage)/home/page.tsx
+++ mst/src/app/(mainpage)/home/page.tsx
@@ -1,34 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { css } from "@emotion/react";
 import Form from "@/components/common/Form/Form";
 import HomeFormContent from "@/components/Home/HomeFormContent";
-import BasicTopBar from "@/components/common/TopBar/BasicTopBar";
-// import Carousel from "react-material-ui-carousel";
 
 import HomeCarousel from "./_components/HomeCarousel";
-import { SUGAR, COFFEE, WATER } from "@/assets/svgs";
-import { userIdState } from "@/recoil/atoms/userIdState";
-import { useRecoilState, useRecoilValue } from "recoil";
 import useUserId from "@/hooks/useUserId";
 
 function Home() {
-  const [index, setIndex] = useState(0);
-  // const [id, setId] = useRecoilState(userIdState);
-  // console.log("현재", id);
-
-  // useEffect(() => {
-  //   const temp = async () => {
-  //     const userId = await getUserIdAPI();
-  //     setId(userId);
-  //   };
-	// 	temp()
-  // }, []);
-
-	// console.log(id)
-	const userId = useUserId()
-	console.log("userID", userId)
+  const userId = useUserId();
+  console.log("userID", userId);
 
   const items = [
     {
@@ -47,7 +27,6 @@ function Home() {
 
   return (
     <div>
-      {/* <BasicTopBar content={"홈페이지"} /> */}
       <Form content={<HomeFormContent />} />
       <HomeCarousel items={items} />
     </div>
